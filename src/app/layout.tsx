a3e8e6e5d0873dbb98af35e5b2f481636614afe0
Hamletsspeak <gamleturusadze@gmail.com>
2025-10-02 16:28:02 +0300

    feat: добавлен пакет @vercel/speed-insights и интеграция в layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "MAGAZIN | Геймерский магазин",
@@ -32,6 +33,7 @@ export default function RootLayout({
         </main>
   <Footer />
   <Analytics />
+  <SpeedInsights />
       </body>
     </html>
   );
